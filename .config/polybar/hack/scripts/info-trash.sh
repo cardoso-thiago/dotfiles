61fcbc1dc5e5a82172c506415753b63dcb4ff11d
cardoso-thiago <thiago.cardoso33@gmail.com>
2021-01-02 00:56:07 -0300

    Polybar
    Adicionando modulo de janelas
    Correção do script do modulo de janelas para minimizar a janela quando
    ela estiver ativa no click do botão esquerdo do mouse
    Removendo o token do Open Weather da configuração
    Adicionando arquivo do módulo da lixeira

---
+++ .config/polybar/hack/scripts/info-trash.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+case "$1" in
+    --clean)
+        rm -rf ~/.local/share/Trash/files
+        rm -rf ~/.local/share/Trash/info
+        mkdir ~/.local/share/Trash/files
+        mkdir ~/.local/share/Trash/info
+        notify-send "Lixeira limpa"
+        ;;
+    *)
+        ls ~/.local/share/Trash/files/ | wc -l
+        ;;
+esac
