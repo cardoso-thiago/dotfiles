4967abe08f1a158522fc4c9d988d0a6475040870
cardoso-thiago <thiago.cardoso33@gmail.com>
2021-01-04 22:28:41 -0300

    Melhoria do script de verificações do yay para não chamar o Zenity sem updates

--- .config/polybar/hack/scripts/yay-prompt-updates.sh
+++ .config/polybar/hack/scripts/yay-prompt-updates.sh
@@ -1,18 +1,23 @@
 #! /bin/bash
 ALL=$(yay -Qu --devel --timeupdate | sed 's/\x1b\[[0-9;]*m//g' | sed 's/->//g' | sed 's/^/TRUE /g')
-UPDATE=$(zenity --list \
-  --width=500 --height=400 \
-  --text="Select updates to install:" \
-  --checklist \
-  --separator=" " \
-  --title="The following packages are outdated" \
-  --column="Update" --column="Package Name" --column="Previous Version" --column="Current Version" \
-  $ALL)
-if [[ -z "${UPDATE// }" ]]
+if [ -z $ALL ]
 then
-    exit 1
+  exit 0
 else
-    RESULT=$(konsole --hide-menubar --hide-tabbar --hold -e "yay -S --noconfirm $UPDATE")
+    UPDATE=$(zenity --list \
+    --width=500 --height=400 \
+    --text="Select updates to install:" \
+    --checklist \
+    --separator=" " \
+    --title="The following packages are outdated" \
+    --column="Update" --column="Package Name" --column="Previous Version" --column="Current Version" \
+    $ALL)
+    if [[ -z "${UPDATE// }" ]]
+    then
+        exit 1
+    else
+        RESULT=$(konsole -e "yay -S $UPDATE")
+    fi
+    echo "Requesting Update: $UPDATE"
 fi
-echo "Requesting Update: $UPDATE"
 exit 0
