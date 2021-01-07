89326d20a740d7c25aa387c7a39da320ac795165
cardoso-thiago <thiago.cardoso33@gmail.com>
2021-01-06 22:09:17 -0300

    Correção script yay

--- .config/polybar/hack/scripts/yay-prompt-updates.sh
+++ .config/polybar/hack/scripts/yay-prompt-updates.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 ALL=$(yay -Qu --devel --timeupdate | sed 's/\x1b\[[0-9;]*m//g' | sed 's/->//g' | sed 's/^/TRUE /g')
-if [ -z $ALL ]
+if [[ -z "${ALL// }" ]]
 then
+  echo "Finalizando, atualizações não encontradas."
   exit 0
 else
+    echo "Vai exibir o diálogo com as opções."
     UPDATE=$(zenity --list \
     --width=500 --height=400 \
     --text="Select updates to install:" \
@@ -12,9 +14,10 @@ else
     --title="The following packages are outdated" \
     --column="Update" --column="Package Name" --column="Previous Version" --column="Current Version" \
     $ALL)
+    echo $UPDATE
     if [[ -z "${UPDATE// }" ]]
     then
-        exit 1
+        exit 0
     else
         RESULT=$(konsole -e "yay -S $UPDATE")
     fi
