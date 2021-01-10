fe15c8abc387bb539e55ac1d1aef5c267beda94c
cardoso-thiago <thiago.cardoso33@gmail.com>
2021-01-09 23:53:11 -0300

    Limpeza do script principal do fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,35 +13,7 @@ set SPACEFISH_CHAR_SYMBOL ❯
 thefuck --alias | source
 alias copydir="pwd | xsel -b"
 alias ofd="xdg-open ."
-alias lc='colorls -1'
-alias lca='colorls -1a'
-alias lcf='colorls -la'
-alias lcg='colorls -la --gs'
-alias l='lc'
 alias cls='clear; echo; echo; seq 1 (tput cols) | sort -R | spark | lolcat; echo; echo'
-
-### Kubectl ###
-alias k='kubectl'
-alias kgp='kubectl get pods'
-alias kgpa='kubectl get pods --all-namespaces'
-alias kdp='kubectl describe pods'
-alias kdelp='kubectl delete pods'
-alias kgs='kubectl get svc'
-alias kgsa='kubectl get svc --all-namespaces'
-alias kds='kubectl describe svc'
-alias kdels='kubectl delete svc'
-alias kgi='kubectl get ingress'
-alias kgia='kubectl get ingress --all-namespaces'
-alias kdi='kubectl describe ingress'
-alias kdeli='kubectl delete ingress'
-alias kcn='kubectl config set-context (kubectl config current-context) --namespace'
-alias kgd='kubectl get deployment'
-alias kgda='kubectl get deployment --all-namespaces'
-alias kdd='kubectl describe deployment'
-alias kdeld='kubectl delete deployment'
-alias kga='kubectl get all'
-alias kgaa='kubectl get all --all-namespaces'
-alias kl='kubectl logs'
 
 ### Functions ###
 function __fish_default_command_not_found_handler
