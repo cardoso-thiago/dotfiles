6d779b91ec40b3b11343c0891103c56c15172cf1
cardoso-thiago <thiago.cardoso33@gmail.com>
2024-03-25 15:55:35 -0300

    Correção da config do Conky

--- .config/conky/Sirius/scripts/playerctl.sh
+++ .config/conky/Sirius/scripts/playerctl.sh
@@ -8,7 +8,7 @@ PCTL=$(playerctl status)
 if [[ ${PCTL} == "" ]]; then
 	echo "Sem Música"
 else
-	playerctl metadata xesam:artist
+	playerctl metadata xesam:artist | awk 'length($0) > 15 {print substr($0, 1, 15) "..."; next} 1'
 fi
 
 exit
