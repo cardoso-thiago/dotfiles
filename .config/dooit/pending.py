dfeaea04085672af453f0f9641ece819d5dba103
cardoso-thiago <thiago.cardoso33@gmail.com>
2024-11-24 18:52:11 -0300

    Remoção do findex e modificação do script do dooit

--- .config/dooit/pending.py
+++ .config/dooit/pending.py
@@ -1,12 +1,16 @@
 from dooit.api import Workspace, Todo, manager
 
-def count_pending_todos():
+def count_pending_tasks():
     manager.connect()
 
     query = Todo.all()
-    pending_todos = [todo for todo in query if todo.is_pending]
+    pending_tasks = len([todo for todo in Todo.all() if todo.is_pending])
+    overdue_tasks = len([todo for todo in Todo.all() if todo.is_overdue])
 
-    return len(pending_todos)
+    if pending_tasks > 0:
+        print(f"\033[38;2;250;179;135m{pending_tasks} tarefa(s) pendente(s).\033[0m")
+        if overdue_tasks > 0:
+            print(f"\033[38;2;224;108;117m{overdue_tasks} tarefa(s) vencida(s).\033[0m")
 
 if __name__ == "__main__":
-    print(f"Tarefas pendentes: {count_pending_todos()}")
+    count_pending_tasks()
