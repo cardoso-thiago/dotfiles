7eefc59b5c2899f228659c411b77425efdc134b9
cardoso-thiago <thiago.cardoso33@gmail.com>
2026-07-04 22:50:19 -0300

    Atualização e limpeza

--- .config/conky/Sargas/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-killall conky
-sleep 2s
-		
-conky -c $HOME/.config/conky/Sargas/Sargas.conf &> /dev/null &
-# conky -c $HOME/.config/conky/Sargas/Sargas.conf
-
-exit
--- .config/conky/Simple/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-killall conky
-sleep 5s
-		
-conky -c $HOME/.config/conky/Simple/Simple.conf &> /dev/null &
-
-exit
